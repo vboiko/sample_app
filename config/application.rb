143d4dee108bb35e1d5850682afb1fd9527d84d1
Vlada Boiko <vladislava.boiko@gmail.com>
2014-02-03 22:49:42 +0200

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,10 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    #config.generators do |g|
+    # config.generators do |g|
     #  g.test_framework  :rspec
     #end
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
   end
 end
